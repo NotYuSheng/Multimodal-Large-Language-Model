9d2281abda2d9f1ad8e2799e285b723b5007466f
Yu Sheng <37260088+NotYuSheng@users.noreply.github.com>
2024-05-27 20:43:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,6 @@ import streamlit as st
 from PIL import Image
 from model import load_model, process_image_and_question
 
-st.markdown(
-    """
-    <style>
-        button[title^=Exit]+div [data-testid=stImage]{
-            text-align: center;
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-            width: 100%;
-        }
-    </style>
-    """, unsafe_allow_html=True
-)
-
 # Streamlit app
 st.title("Multimodal Large Language Model Web App")
 
@@ -27,7 +13,7 @@ if uploaded_image is not None:
     image = Image.open(uploaded_image)
     # Resize the image to a maximum width of 500 pixels
     resized_image = image.resize((500, int(500 * image.height / image.width)))
-    st.image(resized_image, caption="Uploaded Image", output_format='auto')
+    st.image(resized_image, caption="Uploaded Image")
 
 # User question input
 question = st.text_input("Ask a question about the image")
