457683ce9391d0757f72d01ada3d039514e4081f
Yu Sheng <37260088+NotYuSheng@users.noreply.github.com>
2024-06-11 22:37:44 +0800

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -22,6 +22,6 @@ fi
 
 # Start the Streamlit application in the background
 echo "Starting Streamlit application..."
-noup streamlit run --server.enableCORS false --server.enableXsrfProtection false app.py &
+nohup streamlit run --server.enableCORS false --server.enableXsrfProtection false app.py &
 
 echo "All services startup completed"
